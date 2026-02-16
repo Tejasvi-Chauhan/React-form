8e7e0cf2341538eb4c2369dd846dc0b403eef529
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-02-16 23:53:29 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,56 @@
 require("dotenv").config();
 
-const express=require('express')
+const express = require("express");
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors());          
+app.use(cors());
 
 app.use(express.json());
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   let users = [
-  {
-    id: 1,
-    name: "Ravi",
-    email: "ravi@gmail.com",
-    password: "123456",
-    
-  },
+    {
+      id: 1,
+      name: "Ravi",
+      email: "ravi@gmail.com",
+      password: "123456",
+    },
 
-      {
-    id: 2,
-    name: "Amit",
-    email: "amit@gmail.com",
-    password: "password",
-    
-  
-  }
-];
+    {
+      id: 2,
+      name: "Amit",
+      email: "amit@gmail.com",
+      password: "password",
+    },
+  ];
   res.send(users);
-})
+});
 
-app.get("/lib",(req,res)=>{
+app.get("/lib", (req, res) => {
   res.send("Lib");
-})
+});
 
-app.get("/lib",(req,res)=>{
-})
-const user=users.filter((u)=>u.id==2);
+app.get("/lib", (req, res) => {});
+const user = users.filter((u) => u.id == 2);
 console.log(user[0].name);
 
-app.post("/datafor",(req,res)=>{
+app.post("/datafor", (req, res) => {
   console.log(req.body);
-  res.res.status(200).json({message:"Data received successfully"});
+  res.res.status(200).json({ message: "Data received successfully" });
+});
 
-})
-
-
-
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Data received successfully" });
+});
 // const user=users.find((u)=>u.id==2);
 // console.log(user.name);
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening at ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
